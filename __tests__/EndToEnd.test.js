d53da6d95d804504c6ba11c0c70dcf6be387fa44
zhasan90 <zubi_manutd@hotmail.com>
2025-07-27 10:15:00 -0600

    added end2end testing

---
+++ __tests__/EndToEnd.test.js
@@ -0,0 +1,19 @@
+import puppeteer from 'puppeteer';
+
+describe('show/hide an event details', () => {
+    test('An event element is collapsed by default', async () => {
+        const browser = await puppeteer.launch();
+
+        const page = await browser.newPage();
+        await page.goto('http://localhost:5173/'); // If your Vercel app is running in a different port please update it here
+
+        // If your event element has a different selector, use it instead of .event
+        await page.waitForSelector('.event');
+
+        // If your event's details have a different selector, use it instead of .event .details
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeNull();
+
+        await browser.close();
+    });
+});
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -4,22 +4,24 @@ const Event = ({ event }) => {
     const [showDetails, setShowDetails] = useState(false);
     return (
         <li>
-            <h2>{event.summary}</h2>
-            <p>{event.created}</p>
-            <p>{event.location}</p>
-            <button onClick={() => setShowDetails(!showDetails)}>
-                {showDetails ? 'Hide Details' : 'Show Details'}
-            </button>
+            <div className='event'>
+                <h2>{event.summary}</h2>
+                <p>{event.created}</p>
+                <p>{event.location}</p>
+                <button onClick={() => setShowDetails(!showDetails)}>
+                    {showDetails ? 'Hide Details' : 'Show Details'}
+                </button>
 
-            {showDetails ? (
-                <div className="details">
-                    <p>{event.description}</p>
-                    <p>Start: {event.start.dateTime || event.start.date}</p>
-                    <p>End: {event.end.dateTime || event.end.date}</p>
-                </div>
-            ) : (
-                <></>
-            )}
+                {showDetails ? (
+                    <div className="details">
+                        <p>{event.description}</p>
+                        <p>Start: {event.start.dateTime || event.start.date}</p>
+                        <p>End: {event.end.dateTime || event.end.date}</p>
+                    </div>
+                ) : (
+                    <></>
+                )}
+            </div>
         </li>
     );
 }
